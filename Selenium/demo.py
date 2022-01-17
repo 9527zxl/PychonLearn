8f4e0fc2d29c895fcb42f2f688f32121bc6595f8
9527zxl <1127580235@qq.com>
2022-01-17 16:52:23 +0800

    学习

--- Selenium/demo.py
+++ Selenium/demo.py
@@ -1,4 +1,19 @@
-from selenium import webdriver
+from selenium.webdriver import Chrome
+from selenium.webdriver import ChromeOptions
 
-driver = webdriver.Chrome()
-driver.get('https://www.baidu.com/')
+options = ChromeOptions()
+options.add_experimental_option('excludeSwitches', ['enable-automation'])
+options.add_experimental_option('useAutomationExtension', False)
+
+bro = Chrome(options=options)
+bro.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+    "source": """
+    Object.defineProperty(navigator, 'webdriver', {
+      get: () => undefined
+    })
+  """
+})
+
+bro.get("http://cpquery.cnipa.gov.cn/")
+
+bro.find_element('id', 'username1').send_keys('15755188511')
